3d115175591d65c20ed21d77e3a45f669472476e
SlavaLight <byrman.sl@gmail.com>
2024-04-29 00:37:03 +0300

    add file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("**************************\n");
+        System.out.print("****      Визитка     ****\n");
+        System.out.print("****  Имя: \"Slava\"    ****\n");
+        System.out.print("****  Дс:moonlight01  ****\n");
+        System.out.print("**************************");
+    }
+}
